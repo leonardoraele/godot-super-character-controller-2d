d185db1b549678a17989b84386a80ebb80c8caaa
Leonardo Raele <leonardoraele@gmail.com>
2025-12-14 12:36:40 -0300

    enh: Improved SuperconDebugger node

--- src/SuperconDebugger.cs
+++ src/SuperconDebugger.cs
@@ -1,60 +1,71 @@
-using System;
+using System.Linq;
 using Godot;
-using Raele.GodotUtils;
+using Godot.Collections;
 
 namespace Raele.Supercon2D;
 
-public partial class SuperconDebugger : Control
+[Tool]
+public partial class SuperconDebugger : CanvasLayer
 {
-	[Export] private string ToggleVisibilityInputAction = "ui_menu";
-
-    private SuperconBody2D Character => field != null ? field : field = this.RequireAncestor<SuperconBody2D>();
+	[Export] public string ToggleVisibilityInputAction = "ui_menu";
 
 	public override void _Ready()
-    {
-		this.ZIndex = 1000;
-		this.Visible = false;
+	{
+		// this.Visible = false;
 		this.ProcessMode = ProcessModeEnum.Always;
-    }
+		Node node = ResourceLoader.Load<PackedScene>($"res://addons/{nameof(Supercon2D)}/SuperconDebuggerInterface.tscn").Instantiate();
+		node.Set("character", this.GetParent<SuperconBody2D>());
+		this.AddChild(node);
+	}
 
-    public override void _Draw()
-    {
-		float defaultSpeedBarLength = 16;
-		float characterSpeed = this.Character.Velocity.Length();
-		float characterMaxSpeed = 1000f; // TODO // FIXME this.Character.MovementSettings.MaxHorizontalSpeedPxPSec;
-		float totalSpeedBarLength = defaultSpeedBarLength * (characterSpeed / characterMaxSpeed);
-		float cappedSpeedBarLength = Math.Min(defaultSpeedBarLength, totalSpeedBarLength);
-		this.DrawLine(Vector2.Zero, this.Character.Velocity.Normalized() * defaultSpeedBarLength, Colors.Gray, -1, true);
-		this.DrawLine(Vector2.Zero, this.Character.Velocity.Normalized() * totalSpeedBarLength, Colors.Orange, -1, true);
-		this.DrawLine(Vector2.Zero, this.Character.Velocity.Normalized() * cappedSpeedBarLength, Colors.Green, -1, true);
-		this.DrawString(ThemeDB.FallbackFont, Vector2.Zero, $"{this.Character.Velocity.Length():N2} px/s", default, default, 8, Colors.Green);
+	// public override void _Draw()
+	// {
+	// 	float defaultSpeedBarLength = 16;
+	// 	float characterSpeed = this.Character.Velocity.Length();
+	// 	float characterMaxSpeed = 1000f; // TODO // FIXME this.Character.MovementSettings.MaxHorizontalSpeedPxPSec;
+	// 	float totalSpeedBarLength = defaultSpeedBarLength * (characterSpeed / characterMaxSpeed);
+	// 	float cappedSpeedBarLength = Math.Min(defaultSpeedBarLength, totalSpeedBarLength);
+	// 	this.DrawLine(Vector2.Zero, this.Character.Velocity.Normalized() * defaultSpeedBarLength, Colors.Gray, -1, true);
+	// 	this.DrawLine(Vector2.Zero, this.Character.Velocity.Normalized() * totalSpeedBarLength, Colors.Orange, -1, true);
+	// 	this.DrawLine(Vector2.Zero, this.Character.Velocity.Normalized() * cappedSpeedBarLength, Colors.Green, -1, true);
+	// 	this.DrawString(ThemeDB.FallbackFont, Vector2.Zero, $"{this.Character.Velocity.Length():N2} px/s", default, default, 8, Colors.Green);
 
-		Vector2 inputPos = Vector2.Down * 32;
-		this.DrawLine(inputPos, this.Character.InputMapping.MovementInput * defaultSpeedBarLength + inputPos, Colors.Green, -1, true);
-		this.DrawCircle(inputPos, 2, Colors.Green);
-		this.DrawArc(inputPos, defaultSpeedBarLength, 0f, 2 * (float) Math.PI, 360, Colors.Green);
-		this.DrawString(ThemeDB.FallbackFont, inputPos, $"({this.Character.InputMapping.MovementInput.X:N2}, {this.Character.InputMapping.MovementInput.Y:N2}) {this.Character.InputMapping.MovementInput.Length():N2}/1 {this.Character.InputMapping.MovementInput.Angle():N2}º", default, default, 8, Colors.Green);
+	// 	Vector2 inputPos = Vector2.Down * 32;
+	// 	this.DrawLine(inputPos, this.Character.InputMapping.MovementInput * defaultSpeedBarLength + inputPos, Colors.Green, -1, true);
+	// 	this.DrawCircle(inputPos, 2, Colors.Green);
+	// 	this.DrawArc(inputPos, defaultSpeedBarLength, 0f, 2 * (float) Math.PI, 360, Colors.Green);
+	// 	this.DrawString(ThemeDB.FallbackFont, inputPos, $"({this.Character.InputMapping.MovementInput.X:N2}, {this.Character.InputMapping.MovementInput.Y:N2}) {this.Character.InputMapping.MovementInput.Length():N2}/1 {this.Character.InputMapping.MovementInput.Angle():N2}º", default, default, 8, Colors.Green);
 
-		if (this.Character.IsOnFloor()) {
-			this.DrawLine(Vector2.Zero, this.Character.GetFloorNormal() * 8, Colors.White, 1, true);
-		}
-		if (this.Character.IsOnWall()) {
-			this.DrawLine(this.Character.Velocity.Normalized() * 32, this.Character.Velocity.Normalized() * 32 + this.Character.GetWallNormal() * 8, Colors.White, 1, true);
-		}
-		this.DrawString(ThemeDB.FallbackFont, Vector2.Down * 8, this.Character.StateMachine.ActiveState?.Name ?? "null state", default, default, 8, Colors.White);
-    }
+	// 	if (this.Character.IsOnFloor()) {
+	// 		this.DrawLine(Vector2.Zero, this.Character.GetFloorNormal() * 8, Colors.White, 1, true);
+	// 	}
+	// 	if (this.Character.IsOnWall()) {
+	// 		this.DrawLine(this.Character.Velocity.Normalized() * 32, this.Character.Velocity.Normalized() * 32 + this.Character.GetWallNormal() * 8, Colors.White, 1, true);
+	// 	}
+	// 	this.DrawString(ThemeDB.FallbackFont, Vector2.Down * 8, this.Character.StateMachine.ActiveState?.Name ?? "null state", default, default, 8, Colors.White);
+	// }
 
-    public override void _Process(double delta)
-    {
-		if (this.Visible) {
-        	this.QueueRedraw();
-		}
-    }
-
-    public override void _Input(InputEvent @event)
-    {
-        if (@event.IsActionPressed(this.ToggleVisibilityInputAction)) {
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed(this.ToggleVisibilityInputAction)) {
 			this.Visible = !this.Visible;
+			this.GetChildren().ToList().ForEach(child =>
+				child.ProcessMode = this.Visible ? ProcessModeEnum.Inherit : ProcessModeEnum.Disabled
+			);
+			this.GetViewport().SetInputAsHandled();
 		}
-    }
+	}
+
+	public override void _ValidateProperty(Dictionary property)
+	{
+		base._ValidateProperty(property);
+		switch (property["name"].AsString())
+		{
+			case nameof(this.ToggleVisibilityInputAction):
+				InputMap.LoadFromProjectSettings();
+				property["hint"] = (long) PropertyHint.Enum;
+				property["hint_string"] = string.Join(",", InputMap.GetActions());
+				break;
+		}
+	}
 }
