26e92b0fbd356282faf36e6ff4a250e2176f0e0f
Leonardo Raele <leonardoraele@gmail.com>
2026-01-29 22:42:00 -0300

    feat(wip): Fefined PlaneControlComponent with a bunch of new options, but they are not implemented yet

--- src/StateComponents3D/PlaneControlComponent.cs
+++ src/StateComponents3D/PlaneControlComponent.cs
@@ -1,4 +1,3 @@
-// using System;
 using Godot;
 using Raele.GodotUtils.Extensions;
 
@@ -12,7 +11,7 @@ namespace Raele.Supercon.StateComponents3D;
 ///
 /// This component does not not update the character's rotation direction. For that, use the FacingComponent.
 /// </summary>
-[GlobalClass]
+[Tool][GlobalClass]
 public partial class PlaneControlComponent : SuperconStateComponent3D
 {
 	//==================================================================================================================
@@ -28,15 +27,95 @@ public partial class PlaneControlComponent : SuperconStateComponent3D
 	//==================================================================================================================
 
 	[Export] public PlaneOptionsEnum MovementPlane = PlaneOptionsEnum.Floor;
-	[Export(PropertyHint.None, "suffix:m/s")] public float MaxForwardSpeed = 5f;
-	[Export(PropertyHint.None, "suffix:m/s")] public float MaxLateralSpeed = 5f;
-	[Export(PropertyHint.None, "suffix:m/s")] public float MaxBackwardSpeed = 5f;
-	[Export(PropertyHint.None, "suffix:m/s")] public float MaxVerticalSpeed = 5f;
-	[Export(PropertyHint.None, "suffix:m/s²")] public float Acceleration = 10f;
-	[Export(PropertyHint.None, "suffix:m/s²")] public float DragDeceleration = 15f;
-	[Export(PropertyHint.None, "suffix:m/s²")] public float BrakeDeceleration = 50f;
-	[Export(PropertyHint.None, "0,360,radians_as_degrees,suffix:°/s")] public float TurnSpeed = 50f;
-	[Export] public SpeedAboveMaxBehavior SpeedAboveMax = SpeedAboveMaxBehavior.DragIfInputIsNeutral;
+	[Export(PropertyHint.None, "suffix:m/s")] public float MaxSpeed
+		{ get; set { if (value != field) field = /*this.MaxForwardSpeed =*/ value.Clamped(0f, value); } }
+		= 5f;
+	[Export(PropertyHint.None, "suffix:m/s²")] public float Acceleration
+		{ get; set => field = value.Clamped(0f, value); }
+		= 10f;
+	[Export(PropertyHint.None, "suffix:m/s²")] public float Deceleration
+		{ get; set { if (value != field) field = /*this.DragDeceleration =*/ value.Clamped(0f, value); } }
+		= 15f;
+	[Export(PropertyHint.Range, "0,1080,5,radians_as_degrees,or_greater,suffix:°/s")] public float AngularVelocity
+	{
+		get;
+		set
+		{
+			if (value == field)
+				return;
+			// this.TurnMinAngularVelocity = this.TurnMinAngularVelocity.Clamped(0f, value);
+			/*this.TurnMaxAngularVelocity =*/ field = value;
+		}
+	}
+		= Mathf.Pi * 2;
+
+	// [ExportGroup("Rotate Character", "Rotate Character")]
+	// [Export(PropertyHint.GroupEnable)] public bool RotateCharacterEnabled = false;
+	// [Export] public Vector3 ForwardVector = Vector3.Forward;
+	// [Export] public Vector3 UpVector = Vector3.Up;
+
+	// [ExportGroup("Break MaxSpeed")]
+	// [Export(PropertyHint.GroupEnable)] public bool MaxSpeedOptionsEnabled
+	// 	{ get; set { field = value; this.NotifyPropertyListChanged(); } }
+	// 	= false;
+	// [Export(PropertyHint.None, "suffix:m/s")] public float MaxForwardSpeed
+	// 	{ get; set { if (value != field) field = this.MaxSpeed = value.Clamped(0f, value); } }
+	// 	= 5f;
+	// [Export(PropertyHint.None, "suffix:m/s")] public float MaxLateralSpeed
+	// 	{ get; set { field = value.Clamped(0f, value); } }
+	// 	= 5f;
+	// [Export(PropertyHint.None, "suffix:m/s")] public float MaxBackwardSpeed
+	// 	{ get; set { field = value.Clamped(0f, value); } }
+	// 	= 5f;
+	// [Export(PropertyHint.None, "suffix:m/s")] public float MaxVerticalSpeed
+	// 	{ get; set { field = value.Clamped(0f, value); } }
+	// 	= 5f;
+
+	// [ExportGroup("Break Deceleration")]
+	// /// <summary>
+	// /// Determines the deceleration behavior when the character's speed exceeds the maximum speed.
+	// ///
+	// /// If this is higher than <see cref="Deceleration"/>, the character will decelerate faster when above max speed.
+	// /// </summary>
+	// [Export(PropertyHint.GroupEnable)] public bool DecelerationOptionsEnabled
+	// 	{ get; set { field = value; this.NotifyPropertyListChanged(); } }
+	// 	= false;
+	// [Export(PropertyHint.None, "suffix:m/s²")] public float DragDeceleration
+	// 	{ get; set { if (value != field) field = this.Deceleration = value.Clamped(0f, value); } }
+	// 	= 15f;
+	// [Export(PropertyHint.None, "suffix:m/s²")] public float DecelerationAboveMaxSpeed
+	// 	{ get; set { field = value.Clamped(0f, value); } }
+	// 	= 5f;
+
+	// [ExportGroup("Break Angular Velocity", "Turn")]
+	// [Export(PropertyHint.GroupEnable)] public bool AngularVelocityOptionsEnabled
+	// 	{ get; set { field = value; this.NotifyPropertyListChanged(); } }
+	// 	= false;
+	// // TODO Implement dynamic range min/max using _ValidateProperty() instead of setter
+	// [Export(PropertyHint.Range, "0,1080,5,radians_as_degrees,or_greater,suffix:°/s")] public float TurnMinAngularVelocity
+	// 	{ get; set { field = value.Clamped(0f, this.TurnMaxAngularVelocity); } }
+	// 	= Mathf.Pi / 2;
+	// // TODO Implement dynamic range min/max using _ValidateProperty() instead of setter
+	// [Export(PropertyHint.Range, "0,1080,5,radians_as_degrees,or_greater,suffix:°/s")] public float TurnMaxAngularVelocity
+	// 	{ get; set { if (value != field) this.AngularVelocity = field = value.Clamped(this.TurnMinAngularVelocity, float.PositiveInfinity); } }
+	// 	= Mathf.Pi * 2;
+	// [Export(PropertyHint.None, "m/s²")] public float TurnLowerMovementSpeed
+	// 	{ get; set { field = value.Clamped(0f, this.TurnUpperMovementSpeed); } }
+	// 	= 0f;
+	// [Export(PropertyHint.None, "m/s²")] public float TurnUpperMovementSpeed
+	// 	{ get; set { field = value.Clamped(this.TurnLowerMovementSpeed, float.PositiveInfinity); } }
+	// 	= 5f;
+	// // [Export] public bool TurnExtrapolateBeyondMaxSpeed = false;
+	// [Export(PropertyHint.ExpEasing, "attenuation")] public float TurnCurve = 1f;
+
+	// [ExportGroup("Brake On Sharp Turns")]
+	// [Export(PropertyHint.GroupEnable)] public bool SharpTurnOptionsEnabled = false;
+	// [Export(PropertyHint.Range, "0,180,5,radians_as_degrees,suffix:°")] public float SoftBrakeTurnAngle = Mathf.Pi / 3;
+	// [Export(PropertyHint.ExpEasing, "attenuation")] public float SoftBrakeVelocityLoss = 1f;
+	// [Export(PropertyHint.Range, "0,180,5,radians_as_degrees,suffix:°")] public float HardBrakeTurnAngle = Mathf.Pi / 3;
+	// [Export(PropertyHint.None, "suffix:m/s²")] public float HardBrakeDeceleration
+	// 	{ get; set { field = value.Clamped(0f, value); } }
+	// 	= 50f;
 
 	//==================================================================================================================
 		#endregion
@@ -51,27 +130,6 @@ public partial class PlaneControlComponent : SuperconStateComponent3D
 	//==================================================================================================================
 		#region COMPUTED PROPERTIES
 	//==================================================================================================================
-
-	public Plane? ResolveGlobalMovementPlane()
-		=> this.MovementPlane switch
-		{
-			PlaneOptionsEnum.Floor => this.Character?.IsOnFloor() == true
-				&& this.Character.GetLastSlideCollision() is KinematicCollision3D collision
-					? new Plane(collision.GetNormal(), collision.GetPosition())
-					: null,
-			PlaneOptionsEnum.Wall => this.Character?.IsOnWall() == true
-				&& this.Character.GetLastSlideCollision() is KinematicCollision3D collision
-					? new Plane(collision.GetNormal(), collision.GetPosition())
-					: null,
-			PlaneOptionsEnum.Ceiling => this.Character?.IsOnCeiling() == true
-				&& this.Character.GetLastSlideCollision() is KinematicCollision3D collision
-					? new Plane(collision.GetNormal(), collision.GetPosition())
-					: null,
-			PlaneOptionsEnum.PlaneXZ => Plane.PlaneXZ with { D = this.Character?.GlobalPosition.Length() ?? 0},
-			PlaneOptionsEnum.PlaneXY => Plane.PlaneXY with { D = this.Character?.GlobalPosition.Length() ?? 0},
-			PlaneOptionsEnum.PlaneYZ => Plane.PlaneYZ with { D = this.Character?.GlobalPosition.Length() ?? 0},
-			_ => null,
-		};
 
 	//==================================================================================================================
 		#endregion
@@ -94,14 +152,9 @@ public partial class PlaneControlComponent : SuperconStateComponent3D
 		PlaneXZ = 64,
 		PlaneXY = 65,
 		PlaneYZ = 66,
-	}
-
-	public enum SpeedAboveMaxBehavior : sbyte
-	{
-		Clamp = 0,
-		DragIfInputIsNeutral = 24,
-		AlwaysDrag = 32,
-		Brake = 64,
+		NegativePlaneXZ = 78,
+		NegativePlaneXY = 79,
+		NegativePlaneYZ = 80,
 	}
 
 	//==================================================================================================================
@@ -115,15 +168,25 @@ public partial class PlaneControlComponent : SuperconStateComponent3D
 	// 		.AppendIf(false "This node is not configured correctly. Did you forget to assign a required field?")
 	// 		.ToArray();
 
-	// public override void _ValidateProperty(Godot.Collections.Dictionary property)
-	// {
-	// 	base._ValidateProperty(property);
-	// 	switch (property["name"].AsString())
-	// 	{
-	// 		case nameof():
-	// 			break;
-	// 	}
-	// }
+	public override void _ValidateProperty(Godot.Collections.Dictionary property)
+	{
+		base._ValidateProperty(property);
+		switch (property["name"].AsString())
+		{
+			// case nameof(this.MaxSpeed):
+			// 	if (this.MaxSpeedOptionsEnabled)
+			// 		property["usage"] = (long) PropertyUsageFlags.None;
+			// 	break;
+			// case nameof(this.Deceleration):
+			// 	if (this.DecelerationOptionsEnabled)
+			// 		property["usage"] = (long) PropertyUsageFlags.None;
+			// 	break;
+			// case nameof(this.AngularVelocity):
+			// 	if (this.AngularVelocityOptionsEnabled)
+			// 		property["usage"] = (long) PropertyUsageFlags.None;
+			// 	break;
+		}
+	}
 
 	protected override void _ActivityPhysicsProcess(double delta)
 	{
@@ -133,7 +196,7 @@ public partial class PlaneControlComponent : SuperconStateComponent3D
 		if (this.ResolveGlobalMovementPlane() is not Plane plane)
 			return;
 		Vector3 currentVelocity = this.Character.Velocity.Project(plane);
-		double currentSpeed = currentVelocity.Length();
+		float currentSpeed = currentVelocity.Length();
 		bool isMoving = currentSpeed > Mathf.Epsilon;
 		Vector3 currentDirection = isMoving
 			? currentVelocity.Normalized()
@@ -145,26 +208,28 @@ public partial class PlaneControlComponent : SuperconStateComponent3D
 			? Basis.LookingAt(plane.Normal) * new Vector3(rawInput.X, rawInput.Y, 0)
 			: Vector3.Zero;
 		Vector3 newGlobalDirection = isMoving && hasInput
-				? currentDirection.MoveToward(inputDirection, this.TurnSpeed * delta)
+				? currentDirection.MoveToward(inputDirection, this.AngularVelocity * delta)
 			: isMoving ? currentDirection
 			: hasInput ? inputDirection
 			: this.Character.Basis.Forward;
-		Vector3 newLocalDirection = this.Character.GlobalBasis * newGlobalDirection;
-		float maxSpeed = new Vector3(
-				newLocalDirection.X * this.MaxLateralSpeed,
-				newLocalDirection.Y * this.MaxVerticalSpeed,
-				newLocalDirection.Z * (
-					newLocalDirection.Z < 0
-						? this.MaxForwardSpeed
-						: this.MaxBackwardSpeed
+		// Vector3 newLocalDirection = this.Character.GlobalBasis * newGlobalDirection;
+		float maxSpeed = /*this.MaxSpeedOptionsEnabled
+			? new Vector3(
+					newLocalDirection.X * this.MaxLateralSpeed,
+					0,
+					newLocalDirection.Z * (
+						newLocalDirection.Z < 0
+							? this.MaxForwardSpeed
+							: this.MaxBackwardSpeed
+					)
 				)
-			)
-			.Length();
+				.Length()
+			:*/ this.MaxSpeed;
 		float targetSpeed = maxSpeed * inputStrength;
-		float acceleration = targetSpeed > currentSpeed - Mathf.Epsilon ? this.Acceleration
-			: inputStrength.IsZeroApprox() ? this.DragDeceleration
-			: this.BrakeDeceleration;
-		float newSpeed = (float) currentSpeed.MoveToward(targetSpeed, acceleration * delta);
+		float acceleration = targetSpeed > currentSpeed - Mathf.Epsilon
+			? this.Acceleration
+			: this.Deceleration;
+		float newSpeed = currentSpeed.MoveToward(targetSpeed, acceleration * (float) delta);
 		this.Character.Velocity = newGlobalDirection * newSpeed
 			+ this.Character.Velocity.Project(plane.Normal);
 	}
@@ -174,6 +239,30 @@ public partial class PlaneControlComponent : SuperconStateComponent3D
 	//==================================================================================================================
 		#region METHODS
 	//==================================================================================================================
+
+	public Plane? ResolveGlobalMovementPlane()
+		=> this.MovementPlane switch
+		{
+			PlaneOptionsEnum.Floor => this.Character?.IsOnFloor() == true
+				&& this.Character.GetLastSlideCollision() is KinematicCollision3D collision
+					? new Plane(collision.GetNormal(), collision.GetPosition())
+					: null,
+			PlaneOptionsEnum.Wall => this.Character?.IsOnWall() == true
+				&& this.Character.GetLastSlideCollision() is KinematicCollision3D collision
+					? new Plane(collision.GetNormal(), collision.GetPosition())
+					: null,
+			PlaneOptionsEnum.Ceiling => this.Character?.IsOnCeiling() == true
+				&& this.Character.GetLastSlideCollision() is KinematicCollision3D collision
+					? new Plane(collision.GetNormal(), collision.GetPosition())
+					: null,
+			PlaneOptionsEnum.PlaneXZ => Plane.PlaneXZ with { D = this.Character?.GlobalPosition.Length() ?? 0},
+			PlaneOptionsEnum.PlaneXY => Plane.PlaneXY with { D = this.Character?.GlobalPosition.Length() ?? 0},
+			PlaneOptionsEnum.PlaneYZ => Plane.PlaneYZ with { D = this.Character?.GlobalPosition.Length() ?? 0},
+			PlaneOptionsEnum.NegativePlaneXZ => new Plane(Vector3.Down, this.Character?.GlobalPosition ?? Vector3.Zero),
+			PlaneOptionsEnum.NegativePlaneXY => new Plane(Vector3.Forward, this.Character?.GlobalPosition ?? Vector3.Zero),
+			PlaneOptionsEnum.NegativePlaneYZ => new Plane(Vector3.Left, this.Character?.GlobalPosition ?? Vector3.Zero),
+			_ => null,
+		};
 
 	//==================================================================================================================
 		#endregion
